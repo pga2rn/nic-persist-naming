b04db971cbada02b018c3259d3fb8455ee3cbf4c
pga2rn <pga2rn@workstation.t.pga>
2020-04-27 19:44:56 +0900

    genifname.sh: Let the first port of multi-port nic show f0 in device name according to systemd persist nic naming schemes. Fix some small issues.

--- src/genifname.sh
+++ src/genifname.sh
@@ -8,6 +8,7 @@
 # example:
 # /devices/pci0000:00/0000:00:0a.2/net/eth3 (with dev_port 2)
 # enp0s10f2d2
+# meanwhile, function 0 of this multiport nic should be: enp0s10f0
 
 # sys_class_net
 NET_CLASS_DIR="/sys/class/net"
@@ -29,13 +30,20 @@ f=$( printf "%d" \
     "0x$(echo $sf | cut -d'.' -f2)"
     )
 
+# check if the nic has multiple ports
+# generate DEVPATH for port 1, and check whether it exists or not
+p1pci_slot=$(echo $pci_slot | sed -e 's@\.[0-9a-fA-F]\{1,\}@\.1@')
+p1dev_path=$(echo $DEVPATH | sed -e "s@$pci_slot@\|@" | cut -d'|' -f1)$p1pci_slot
+# if the dev_path for nic port 1 exists, we treat this nic as an multi ports nic.
+[ -d "/sys$p1dev_path" ] && mp=1
+
 # get dev_port
 d=$(cat $NET_CLASS_DIR/$DEVNAME/dev_port)
 
 # build the ID_NET_NAME_PATH
 ID_NET_NAME_PATH=enp${p}s${s}
-[ $f != 0 ] && ID_NET_NAME_PATH=${ID_NET_NAME_PATH}f${f}
-[ -n $d -a $d != 0 ] && ID_NET_NAME_PATH=${ID_NET_NAME_PATH}d$d
+[ "$f" != "0" -o "$mp" == "1" ] && ID_NET_NAME_PATH=${ID_NET_NAME_PATH}f$f
+[ -n "$d" -a "$d" != "0" ] && ID_NET_NAME_PATH=${ID_NET_NAME_PATH}d$d
 
 # export new dev name
 echo $ID_NET_NAME_PATH
