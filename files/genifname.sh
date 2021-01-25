7732d28b8ec7c08da7f0ea1e9de690bbbff5ebf4
pga2rn <aaronpigybd@gmail.com>
2021-01-25 22:06:06 +0900

    Refine genifname.sh; use busybox ash shell instead of bash.

--- files/genifname.sh
+++ files/genifname.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # needed env vars
 # DEVPATH, DEVNAME
@@ -15,36 +15,33 @@
 NET_CLASS_DIR="/sys/class/net"
 # ethernet device
 NET_PREFIX="en"
+# helper
+HEX="[0-9a-fA-F]"
 
 # get pci slot
-pci_slot=$(echo $DEVPATH | sed -e "s@/@\n@g" | grep "[0-9a-fA-F]\{4\}:[0-9a-fA-F]\{2\}" | tail -n1)
+pci_slot=$(echo $DEVPATH | grep -o "$HEX\{4\}:$HEX\{2,\}:$HEX\{2,\}.$HEX\+")
 # pslot: 0000:xx:xx.x
-p=$( printf "%d" \
-    "0x$(echo $pci_slot | cut -d':' -f2)"
-    )
+p=$(printf "%d" "0x$(echo $pci_slot | cut -d':' -f2)")
 # slot and function
 sf=$(echo $pci_slot | cut -d':' -f3)
-s=$( printf "%d" \
-    "0x$(echo $sf | cut -d'.' -f1)"
-    )
-f=$( printf "%d" \
-    "0x$(echo $sf | cut -d'.' -f2)"
-    )
+s=$(printf "%d" "0x${sf%%.*}")
+f=$(printf "%d" "0x${sf##*.}")
 
 # check if the nic has multiple ports, 
 # generate DEVPATH for port 1, and check whether it exists or not.
-p1pci_slot=$(echo $pci_slot | sed -e 's@\.[0-9a-fA-F]\{1,\}@\.1@')
+p1pci_slot=${pci_slot%%.*}.1
 p1dev_path=$(echo $DEVPATH | sed -e "s@$pci_slot@\|@" | cut -d'|' -f1)$p1pci_slot
 # if the dev_path for nic port 1 exists, we treat this nic as an multi ports nic.
-[ -n "$p1dev_path" -a -d "/sys$p1dev_path" ] && mp=1
+[ -n "$p1dev_path" ] && [ -d "/sys$p1dev_path" ] && mp=1
 
 # get dev_port
-d=$(cat $NET_CLASS_DIR/$DEVNAME/dev_port)
+[ -e $NET_CLASS_DIR/$DEVNAME/dev_port ] && \
+        d=$(cat $NET_CLASS_DIR/$DEVNAME/dev_port) || d=0
 
 # build the ID_NET_NAME_PATH
 ID_NET_NAME_PATH=enp${p}s${s}
-[ "$f" != "0" -o "$mp" == "1" ] && ID_NET_NAME_PATH=${ID_NET_NAME_PATH}f$f
-[ -n "$d" -a "$d" != "0" ] && ID_NET_NAME_PATH=${ID_NET_NAME_PATH}d$d
+[ "$mp" = "1" ] && ID_NET_NAME_PATH=${ID_NET_NAME_PATH}f$f
+! [ "$d" = "0" ] && ID_NET_NAME_PATH=${ID_NET_NAME_PATH}d$d
 
 # export new dev name
 echo $ID_NET_NAME_PATH
