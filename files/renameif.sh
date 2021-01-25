a29862218c6a891affc3ca1550c4a83901b658f9
pga2rn <aaronpigybd@gmail.com>
2021-01-25 22:31:08 +0900

    Transplant renameif.sh to use ash shell.

--- files/renameif.sh
+++ files/renameif.sh
@@ -1,25 +1,20 @@
-#!/bin/bash
+#!/bin/ash
 
 #
 LIB="/usr/bin/genifname.sh"
 NET_CLASS_DIR="/sys/class/net"
 
-LOGGER="logger -t renameif "
-IP="/sbin/ip "
+LOGGER="logger -t nic-persist-naming "
 
 # scan through all interfaces and try to rename all the interfaces with ethX naming.
-nr_if=($($IP l | grep -o -e ' eth[0-9]\{1,\}:' | sed -e "s@:@@g"))
-
-# try to rename the interfaces we found.
-for i in ${nr_if[@]}
+echo "$(ip l | grep -o "eth[0-9]\+")" | while read i
 do
-    # strip the leading "/sys" from the DEVPATH retrieved from init.d script.
-    export DEVPATH=$(readlink -f $NET_CLASS_DIR/$i | sed -e 's@/sys@@')
-    export DEVNAME=$i
-    NNAME=$(env "$LIB")
-    $LOGGER "$LIB: finished. New device name is $NNAME."
-    $IP l set $i name $NNAME || \
-        $LOGGER "ERROR! Failed to rename $i."
+	# strip the leading "/sys" from the DEVPATH retrieved from init.d script.
+	export DEVPATH=$(readlink -f $NET_CLASS_DIR/$i | sed -e 's@/sys@@')
+	export DEVNAME=$i
+	NNAME=$(env "$LIB")
+	$LOGGER "$LIB: New device name is $NNAME."
+	ip l set $i name $NNAME || $LOGGER "ERROR! Failed to rename $i."
 done
 
 exit 0
